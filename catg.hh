5b8477f1d1514deca4d3c78a08278c90108834a0
bitsofcotton <22259589+bitsofcotton@users.noreply.github.com>
2021-08-28 03:17:09 +0900

    invariant sign fix.

--- catg.hh
+++ catg.hh
@@ -328,8 +328,8 @@ template <typename T, typename feeder> inline T P012L<T,feeder>::next(const T& i
     if(MM < orth) {
       const auto& q(pp[i].first);
       res = q.size() ? revertProgramInvariant<T>(make_pair(
-          (q.dot(vdp.first) - q[varlen - 1] * vdp.first[varlen - 1]) /
-            q[varlen - 1], vdp.second))
+          - (q.dot(vdp.first) - q[varlen - 1] * vdp.first[varlen - 1]) /
+              q[varlen - 1], vdp.second))
         : work[work.size() - 1];
     }
   }

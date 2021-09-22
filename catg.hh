7f5b45d6e85ba46aec2ab3b6862f71fd768a9c2f
bitsofcotton <22259589+bitsofcotton@users.noreply.github.com>
2021-09-22 22:57:23 +0900

    offset isn't zero.

--- catg.hh
+++ catg.hh
@@ -323,7 +323,8 @@ template <typename T, typename feeder> inline T P012L<T,feeder>::next(const T& i
     work[work.size() - 1] = zero;
     res += work[work.size() - 1] = score * (q.size()
       ? revertProgramInvariant<T>(make_pair(
-          - q.dot(vdp.first) / q[varlen - 1], vdp.second)) : T(0));
+          - (q.dot(vdp.first) - q[varlen - 1] * vdp.first[varlen - 1])
+          / q[varlen - 1], vdp.second)) : T(0));
     sscore += abs(score);
   }
   return res * M / sscore;
